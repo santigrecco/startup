1b30b8e21795985b57e4308300768d12e7d0c5ef
Santiago Grecco <santiago.grecco@globant.com>
2016-11-03 18:13:35 -0300

    added cookies and start movie details

---
+++ 03-MVC-AngularJS/classes/movie.js
@@ -0,0 +1,5 @@
+function Movie(title, year, description){
+    this.title =  title;
+    this.year = year;
+    this.description = description;
+}
--- 03-MVC-AngularJS/controller/controller.js
+++ 03-MVC-AngularJS/controller/controller.js
@@ -1,24 +1,33 @@
-app.controller("controller",["$scope", "$cookies", function(scope, $cookies){
-  $cookies.put('something', 'something');
-  var value = $cookies.get('something');
-  console.log(value);
+app.controller("controller", ["$scope","$cookies",function(scope, cookies){
+  if(cookies.getObject('movies') != undefined){
+    scope.movies = cookies.getObject('movies');
+  }else{
+    scope.movies = [];
+  }
+  scope.title;
+  scope.year;
+  scope.description;
+  scope.santi = 'santi';
 
-  // if($cookies.movies != undefined){
-  //   scope.movies = cookies.movies;
-  // }else{
-  //   scope.movies = [];
-  // }
-  // scope.movieToAdd;
-  // scope.santi = 'santi';
-  //
-  // scope.add = function(){
-  //   if(scope.movieToAdd != ''){
-  //     scope.movies.push(scope.movieToAdd);
-  //     scope.movieToAdd ='';
-  //     cookies.movies = scope.movies;
-  //
-  //   }
-  // }
+  scope.add = function(){
+    if(scope.movieToAdd != ''){
+      var movieToAdd = new Movie(scope.title, scope.year, scope.description);
+      scope.movies.push(movieToAdd);
+      scope.title ='';
+      scope.year ='';
+      scope.description= '';
+      cookies.putObject('movies',scope.movies);
+    }
+  }
 
 
 }]);
+
+
+
+
+app.filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
